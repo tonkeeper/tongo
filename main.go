5dc43f1ccade24b0bf5570202ce086430afdfafb
altox <xeroxaltox@gmail.com>
2022-02-18 21:14:10 +0300

    wip: more work on tvm reflection

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import "C"
 import (
 	"encoding/json"
 	"fmt"
+	"tongo/tvm"
 )
 
 // #cgo darwin CFLAGS: -I ./lib/
@@ -11,29 +12,20 @@ import (
 // #include "./lib/libvm-exec-lib.h"
 import "C"
 
-type TvmStackEntry interface{}
-
-type TvmStackEntryNull struct {
-	Type string `json:"type"`
-}
-
-type TVMStackEntryInt struct {
-	Type  string `json:"type"`
-	Value string `json:"value"`
-}
-
 type TVMExecutionResult struct {
-	ExitCode       int             `json:"exit_code"`
-	GasConsumed    int             `json:"gas_consumed"`
-	DataCell       string          `json:"data_cell"`
-	ActionListCell string          `json:"action_list_cell"`
-	Logs           string          `json:"logs"`
-	Stack          []TvmStackEntry `json:"stack"`
+	ExitCode       int                 `json:"exit_code"`
+	GasConsumed    int                 `json:"gas_consumed"`
+	DataCell       string              `json:"data_cell"`
+	ActionListCell string              `json:"action_list_cell"`
+	Logs           string              `json:"logs"`
+	Stack          []tvm.TvmStackEntry `json:"stack"`
 }
 
 func main() {
 
-	config := `{"function_selector":117759,"init_stack":[{"type":"int","value":"123"},{"type":"int","value":"123"}],"code":"te6cckEBBAEAGwABFP8A9KQT9LzyyAsBAgFiAwIAB6GX/0EAAtCnICBl","data":"te6cckEBAQEAAgAAAEysuc0=","time":1645034711694}`
+	//config := `{"function_selector":117759,"init_stack":[{"type":"int","value":"123"},{"type":"int","value":"123"}],"code":"te6cckEBBAEAGwABFP8A9KQT9LzyyAsBAgFiAwIAB6GX/0EAAtCnICBl","data":"te6cckEBAQEAAgAAAEysuc0=","time":1645034711694}`
+	//config := `{"function_selector":117759,"init_stack":[{"type":"int","value":"123"},{"type":"int","value":"123"}],"code":"te6cckEBBAEAJAABFP8A9KQT9LzyyAsBAgFiAwIAGaGX/wIGE5GWP5KzQAMAAtA1XPLA","data":"te6cckEBAQEAAgAAAEysuc0=","time":1645206666065}
+	config := `{"function_selector":117759,"init_stack":[{"type":"int","value":"123"},{"type":"int","value":"123"}],"code":"te6cckEBBAEAKAABFP8A9KQT9LzyyAsBAgFiAwIAIaGX/wIGE5GWP5KzQOLk3gQlAALQeuKz9Q==","data":"te6cckEBAQEAAgAAAEysuc0=","time":1645207136290}`
 
 	res := C.vm_exec(C.int(len(config)), C.CString(config))
 	resJson := C.GoString(res)
@@ -42,6 +34,9 @@ func main() {
 	err := json.Unmarshal([]byte(resJson), &executeResult)
 
 	fmt.Println(err)
-	fmt.Println(executeResult.Stack[0])
+	fmt.Println(executeResult.Stack[2].TupleVal[0].IntVal)
+	fmt.Println(executeResult.Stack[2].TupleVal[1].IntVal)
+	//parse := executeResult.Stack[1].CellVal.BeginParse()
+	//fmt.Println(parse.ReadUint(32))
 	//fmt.Print("Hello world!")
 }
--- tvm/tvmStackEntry.go
+++ tvm/tvmStackEntry.go
@@ -1,17 +1,27 @@
 package tvm
 
-import "math/big"
+import (
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"math/big"
+	"tongo/boc"
+)
 
 type EntryType int
 
 const (
 	Int EntryType = iota
 	Null
+	Cell
+	Tuple
 )
 
 type TvmStackEntry struct {
-	Type   EntryType
-	IntVal big.Int
+	Type     EntryType
+	IntVal   big.Int
+	CellVal  *boc.Cell
+	TupleVal []*TvmStackEntry
 }
 
 func NewIntStackEntry(val big.Int) TvmStackEntry {
@@ -25,4 +35,95 @@ func NewNullStackEntry() TvmStackEntry {
 	return TvmStackEntry{
 		Type: Null,
 	}
+}
+
+func (e *TvmStackEntry) Int() big.Int {
+	return e.IntVal
+}
+
+func (e *TvmStackEntry) Cell() *boc.Cell {
+	return e.CellVal
+}
+
+func (e *TvmStackEntry) IsNull() bool {
+	return e.Type == Null
+}
+
+func (e *TvmStackEntry) IsInt() bool {
+	return e.Type == Int
+}
+
+func (e *TvmStackEntry) IsCell() bool {
+	return e.Type == Cell
+}
+
+func (e *TvmStackEntry) IsTuple() bool {
+	return e.Type == Cell
+}
+
+func (e *TvmStackEntry) UnmarshalJSON(data []byte) error {
+	var m map[string]json.RawMessage
+
+	//m := map[string]string{}
+
+	err := json.Unmarshal(data, &m)
+	if err != nil {
+		return err
+	}
+
+	var entryType string
+	err = json.Unmarshal(m["type"], &entryType)
+	if err != nil {
+		return err
+	}
+
+	if entryType == "int" {
+		var intEntry struct {
+			Type  string `json:"type"`
+			Value string `json:"value"`
+		}
+		err = json.Unmarshal(data, &intEntry)
+		if err != nil {
+			return err
+		}
+
+		e.Type = Int
+		e.IntVal.SetString(intEntry.Value, 10)
+	} else if entryType == "null" {
+		e.Type = Null
+	} else if entryType == "tuple" {
+		var tupleEntry struct {
+			Type  string           `json:"type"`
+			Value []*TvmStackEntry `json:"value"`
+		}
+		err = json.Unmarshal(data, &tupleEntry)
+		if err != nil {
+			return err
+		}
+		e.Type = Tuple
+		e.TupleVal = tupleEntry.Value
+	} else if entryType == "cell" {
+		var cellEntry struct {
+			Type  string `json:"type"`
+			Value string `json:"value"`
+		}
+		err = json.Unmarshal(data, &cellEntry)
+		if err != nil {
+			return err
+		}
+
+		e.Type = Cell
+		cellData, err := base64.StdEncoding.DecodeString(cellEntry.Value)
+		if err != nil {
+			return err
+		}
+		parsedBoc, err := boc.DeserializeBoc(cellData)
+		if err != nil {
+			return err
+		}
+		e.CellVal = parsedBoc[0]
+	} else {
+		return errors.New("unknown stack entry type")
+	}
+	return nil
 }

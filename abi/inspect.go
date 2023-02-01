058d9b7297715863307a719dbeff0c712ae03f34
aleksej.paschenko <aleksej.paschenko@gmail.com>
2023-02-01 13:14:17 +0300

    InspectCode to support wallet-related interfaces

--- abi/inspect.go
+++ abi/inspect.go
@@ -2,8 +2,10 @@ package abi
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/tonkeeper/tongo"
+	"github.com/tonkeeper/tongo/boc"
 )
 
 type MethodInvocation struct {
@@ -18,7 +20,6 @@ type InterfaceIntrospection struct {
 
 type ContractIntrospection struct {
 	// Interfaces is a list of interfaces implemented by a contract.
-	// For an interface to be implemented means there is at least one successfully executed get method.
 	Interfaces map[ContractInterface]InterfaceIntrospection
 }
 
@@ -26,20 +27,32 @@ type ContractIntrospection struct {
 // The contract is not provided directly,
 // instead, it is expected that "executor" has been created with knowledge of the contract's code and data.
 // Executor must be ready to execute multiple different methods and must not rely on a particular order of execution.
-func InspectContract(ctx context.Context, executor Executor, reqAccountID tongo.AccountID) (*ContractIntrospection, error) {
-	results := make(map[ContractInterface]InterfaceIntrospection)
+func InspectContract(ctx context.Context, code []byte, executor Executor, reqAccountID tongo.AccountID) (*ContractIntrospection, error) {
+	if len(code) == 0 {
+		return &ContractIntrospection{}, nil
+	}
+	hash, err := getCodeHash(code)
+	if err != nil {
+		return nil, err
+	}
+	implemented := make(map[ContractInterface]InterfaceIntrospection)
+	for _, iface := range walletsByHashCode[hash.Hex()] {
+		implemented[iface] = InterfaceIntrospection{
+			GetMethods: map[string]MethodInvocation{},
+		}
+	}
 	for _, method := range methodInvocationOrder {
 		typeHint, result, err := method.InvokeFn(ctx, executor, reqAccountID)
 		if err != nil {
 			continue
 		}
 		for _, ifaceName := range method.ImplementedBy {
-			intr, ok := results[ifaceName]
+			intr, ok := implemented[ifaceName]
 			if !ok {
 				intr = InterfaceIntrospection{
 					GetMethods: map[string]MethodInvocation{},
 				}
-				results[ifaceName] = intr
+				implemented[ifaceName] = intr
 			}
 			intr.GetMethods[method.Name] = MethodInvocation{
 				Result:   result,
@@ -48,6 +61,25 @@ func InspectContract(ctx context.Context, executor Executor, reqAccountID tongo.
 		}
 	}
 	return &ContractIntrospection{
-		Interfaces: results,
+		Interfaces: implemented,
 	}, nil
+}
+
+func getCodeHash(code []byte) (*tongo.Bits256, error) {
+	cells, err := boc.DeserializeBoc(code)
+	if err != nil {
+		return nil, err
+	}
+	if len(cells) == 0 {
+		return nil, fmt.Errorf("failed to find a root cell")
+	}
+	h, err := cells[0].Hash()
+	if err != nil {
+		return nil, err
+	}
+	var hash tongo.Bits256
+	if err = hash.FromBytes(h); err != nil {
+		return nil, err
+	}
+	return &hash, nil
 }

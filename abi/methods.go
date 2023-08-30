c49f85603b38b4b28bbaee8b0bc2d4041b7cd10d
aleksej.paschenko <aleksej.paschenko@gmail.com>
2023-08-30 13:42:09 +0300

    GetDomainResult.EscapedDomain to return escaped domain

--- abi/methods.go
+++ abi/methods.go
@@ -5,7 +5,15 @@ import (
 )
 
 func (res GetFullDomainResult) EscapedDomain() string {
-	sl := strings.Split(res.Domain, "\u0000")
+	return escapedDomain(res.Domain)
+}
+
+func (res GetDomainResult) EscapedDomain() string {
+	return escapedDomain(res.Domain)
+}
+
+func escapedDomain(domain string) string {
+	sl := strings.Split(domain, "\u0000")
 	for i := 0; i < len(sl)/2; i++ {
 		sl[i], sl[len(sl)-i-1] = sl[len(sl)-i-1], sl[i]
 	}

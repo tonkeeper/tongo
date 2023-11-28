1d139dc95f4e4e480126253b86e2b5bce98e0f58
aleksej.paschenko <aleksej.paschenko@gmail.com>
2023-11-28 17:03:46 +0300

    Add liteapi.ConvertSendMessagePayloadToMessage

--- liteapi/utils.go
+++ liteapi/utils.go
@@ -9,23 +9,30 @@ import (
 
 // VerifySendMessagePayload verifies that the given payload is an external message ready to be sent to the blockchain.
 func VerifySendMessagePayload(payload []byte) error {
+	_, err := ConvertSendMessagePayloadToMessage(payload)
+	return err
+}
+
+// ConvertSendMessagePayloadToMessage converts the given payload to a tlb.Message.
+// It also verifies that the message is an external message ready to be sent to the blockchain.
+func ConvertSendMessagePayloadToMessage(payload []byte) (*tlb.Message, error) {
 	roots, err := boc.DeserializeBoc(payload)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	if len(roots) != 1 {
-		return fmt.Errorf("external message is not a valid bag of cells")
+		return nil, fmt.Errorf("external message is not a valid bag of cells")
 	}
 	root := roots[0]
 	if root.Level() != 0 {
-		return fmt.Errorf("external message must have zero level")
+		return nil, fmt.Errorf("external message must have zero level")
 	}
 	var msg tlb.Message
 	if err := tlb.Unmarshal(root, &msg); err != nil {
-		return fmt.Errorf("external message is not a tlb.Message")
+		return nil, fmt.Errorf("external message is not a tlb.Message")
 	}
 	if msg.Info.SumType != "ExtInMsgInfo" {
-		return fmt.Errorf("external message must begin with ext_in_msg_info$10")
+		return nil, fmt.Errorf("external message must begin with ext_in_msg_info$10")
 	}
-	return nil
+	return &msg, nil
 }

50597c096699b8a9ffd624e8e105dcfdbea13328
Denis Subbotin <denis@subbot.in>
2024-05-25 01:48:52 +0300

    add precompiled

---
+++ tvm/precompiled/precompiled.go
@@ -0,0 +1,27 @@
+package precompiled
+
+import (
+	"github.com/tonkeeper/tongo/boc"
+	"github.com/tonkeeper/tongo/tlb"
+	"github.com/tonkeeper/tongo/ton"
+)
+
+type MethodCode struct {
+	MethodID int
+	CodeHash [32]byte
+}
+
+type tvmPrecompiled func(data *boc.Cell, args tlb.VmStack) (tlb.VmStack, error)
+
+var KnownMethods = map[MethodCode]tvmPrecompiled{
+	//get_pow_params gram miner
+	{MethodID: 101616, CodeHash: ton.MustParseHash("ccae6ffb603c7d3e779ab59ec267ffc22dc1ebe0af9839902289a7a83e4c00f1")}: getPowParamsGram,
+
+	//wallet v4r2 seqno
+	{MethodID: 85143, CodeHash: ton.MustParseHash("feb5ff6820e2ff0d9483e7e0d62c817d846789fb4ae580c878866d959dabd5c0")}: walletv4r2seqno,
+	{MethodID: 81467, CodeHash: ton.MustParseHash("feb5ff6820e2ff0d9483e7e0d62c817d846789fb4ae580c878866d959dabd5c0")}: walletv4r2SubwalletID,
+	{MethodID: 78748, CodeHash: ton.MustParseHash("feb5ff6820e2ff0d9483e7e0d62c817d846789fb4ae580c878866d959dabd5c0")}: walletv4r2publicKey,
+
+	//jetton v1 get_wallet_data
+	//{MethodID: 97026, CodeHash: ton.MustParseHash("beb0683ebeb8927fe9fc8ec0a18bc7dd17899689825a121eab46c5a3a860d0ce")}: jettonV1getWalletData,
+}

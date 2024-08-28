8ba209256076c9f905c1b6897f45a2c25f563db1
Alexey Kostenko <a.kos@tonkeeper.com>
2024-08-28 20:45:01 +0300

    implement verify proof chain

---
+++ liteapi/account.go
@@ -0,0 +1,143 @@
+package liteapi
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/tonkeeper/tongo/boc"
+	"github.com/tonkeeper/tongo/tlb"
+	"github.com/tonkeeper/tongo/ton"
+)
+
+// GetAccountWithProof
+// For safe operation, always use GetAccountWithProof with WithBlock(proofedBlock ton.BlockIDExt), as the proof of masterchain cashed blocks is not implemented yet!
+func (c *Client) GetAccountWithProof(ctx context.Context, accountID ton.AccountID) (*tlb.ShardAccount, *tlb.ShardStateUnsplit, error) {
+	res, err := c.GetAccountStateRaw(ctx, accountID) // TODO: add proof check for masterHead
+	if err != nil {
+		return nil, nil, err
+	}
+	blockID := res.Id.ToBlockIdExt()
+	if len(res.Proof) == 0 {
+		return nil, nil, errors.New("empty proof")
+	}
+
+	var blockHash ton.Bits256
+	if (accountID.Workchain == -1 && blockID.Workchain == -1) || blockID == res.Shardblk.ToBlockIdExt() {
+		blockHash = blockID.RootHash
+	} else {
+		if len(res.ShardProof) == 0 {
+			return nil, nil, errors.New("empty shard proof")
+		}
+		if res.Shardblk.RootHash == [32]byte{} { // TODO: how to check for empty shard?
+			return nil, nil, errors.New("shard block not passed")
+		}
+		shardHash := ton.Bits256(res.Shardblk.RootHash)
+		if _, err := checkShardInMasterProof(blockID, res.ShardProof, accountID.Workchain, shardHash); err != nil {
+			return nil, nil, fmt.Errorf("shard proof is incorrect: %w", err)
+		}
+		blockHash = shardHash
+	}
+	cellsMap := make(map[[32]byte]*boc.Cell)
+	if len(res.State) > 0 {
+		stateCells, err := boc.DeserializeBoc(res.State)
+		if err != nil {
+			return nil, nil, fmt.Errorf("state deserialization failed: %w", err)
+		}
+		hash, err := stateCells[0].Hash256()
+		if err != nil {
+			return nil, nil, fmt.Errorf("get hash err: %w", err)
+		}
+		cellsMap[hash] = stateCells[0]
+	}
+	shardState, err := checkBlockShardStateProof(res.Proof, blockHash, cellsMap)
+	if err != nil {
+		return nil, nil, fmt.Errorf("incorrect block proof: %w", err)
+	}
+	values := shardState.ShardStateUnsplit.Accounts.Values()
+	keys := shardState.ShardStateUnsplit.Accounts.Keys()
+	for i, k := range keys {
+		if k == accountID.Address {
+			return &values[i], shardState, nil
+		}
+	}
+	if len(res.State) == 0 {
+		return &tlb.ShardAccount{Account: tlb.Account{SumType: "AccountNone"}}, shardState, nil
+	}
+	return nil, nil, errors.New("invalid account state")
+}
+
+func checkShardInMasterProof(master ton.BlockIDExt, shardProof []byte, workchain int32, shardRootHash ton.Bits256) (*tlb.McStateExtra, error) {
+	shardState, err := checkBlockShardStateProof(shardProof, master.RootHash, nil)
+	if err != nil {
+		return nil, fmt.Errorf("check block proof failed: %w", err)
+	}
+	if !shardState.ShardStateUnsplit.Custom.Exists {
+		return nil, fmt.Errorf("not a masterchain block")
+	}
+	stateExtra := shardState.ShardStateUnsplit.Custom.Value.Value
+	keys := stateExtra.ShardHashes.Keys()
+	values := stateExtra.ShardHashes.Values()
+	for i, k := range keys {
+		binTreeValues := values[i].Value.BinTree.Values
+		for _, b := range binTreeValues {
+			switch b.SumType {
+			case "Old":
+				if int32(k) == workchain && ton.Bits256(b.Old.RootHash) == shardRootHash {
+					return &stateExtra, nil
+				}
+			case "New":
+				if int32(k) == workchain && ton.Bits256(b.New.RootHash) == shardRootHash {
+					return &stateExtra, nil
+				}
+			}
+		}
+	}
+	return nil, fmt.Errorf("required shard hash not found in proof")
+}
+
+func checkBlockShardStateProof(proof []byte, blockRootHash ton.Bits256, cellsMap map[[32]byte]*boc.Cell) (*tlb.ShardStateUnsplit, error) {
+	proofCells, err := boc.DeserializeBoc(proof)
+	if err != nil {
+		return nil, err
+	}
+	if len(proofCells) != 2 {
+		return nil, errors.New("must be two root cells")
+	}
+	newStateHash, err := checkBlockProof(*proofCells[0], blockRootHash)
+	if err != nil {
+		return nil, fmt.Errorf("incorrect block proof: %w", err)
+	}
+	var stateProof struct {
+		Proof tlb.MerkleProof[tlb.ShardStateUnsplit]
+	}
+	decoder := tlb.NewDecoder()
+	if cellsMap != nil {
+		decoder = decoder.WithPrunedResolver(func(hash tlb.Bits256) (*boc.Cell, error) {
+			cell, ok := cellsMap[hash]
+			if ok {
+				return cell, nil
+			}
+			return nil, errors.New("not found")
+		})
+	}
+	err = decoder.Unmarshal(proofCells[1], &stateProof)
+	if err != nil {
+		return nil, err
+	}
+	if stateProof.Proof.VirtualHash != *newStateHash {
+		return nil, errors.New("invalid virtual hash")
+	}
+	return &stateProof.Proof.VirtualRoot, nil
+}
+
+func checkBlockProof(proof boc.Cell, blockRootHash ton.Bits256) (*tlb.Bits256, error) {
+	var res tlb.MerkleProof[tlb.Block]
+	err := tlb.Unmarshal(&proof, &res) // merkle hash and depth checks inside
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal block proof: %w", err)
+	}
+	if ton.Bits256(res.VirtualHash) != blockRootHash {
+		return nil, fmt.Errorf("invalid block root hash")
+	}
+	return &res.VirtualRoot.StateUpdate.ToHash, nil // return new_hash field of MerkleUpdate of ShardState
+}
---
+++ ton/prng_test.go
@@ -0,0 +1,174 @@
+package ton
+
+import (
+	"reflect"
+	"testing"
+)
+
+func createValidatorPRNG() (*ValidatorPRNG, error) {
+	return NewValidatorPRNG(nil, 0x8000000000000000, -1, 0)
+}
+
+func TestNextUInt64(t *testing.T) {
+	tests := []struct {
+		name  string
+		times int
+		// expected output
+		outputs []uint64
+	}{
+		{
+			name:    "test next uint64 1 time",
+			times:   1,
+			outputs: []uint64{6186953295200455061},
+		},
+		{
+			name:  "test next uint64 5 times",
+			times: 5,
+			outputs: []uint64{
+				6186953295200455061, 9716249430906648876, 893850564141714240, 16362499097668570104,
+				7550721807492789767,
+			},
+		},
+		{
+			name:  "test next uint64 10 times",
+			times: 10,
+			outputs: []uint64{
+				6186953295200455061, 9716249430906648876, 893850564141714240, 16362499097668570104,
+				7550721807492789767, 8027788155046975774, 2198044665159296191, 15889925754150310949,
+				2854201576873883948, 3908958851740847745,
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			prng, err := createValidatorPRNG()
+			if err != nil {
+				t.Fatalf("cannot create validator prng: %v", err)
+			}
+			output := make([]uint64, test.times)
+			for i := 0; i < test.times; i++ {
+				output[i] = prng.NextUInt64()
+			}
+			if !reflect.DeepEqual(output, test.outputs) {
+				t.Errorf("incorrect values: got %v, want %v", output, test.outputs)
+			}
+		})
+	}
+}
+
+func TestNextRanged(t *testing.T) {
+	tests := []struct {
+		name string
+		rng  uint64
+		// expected output
+		output uint64
+	}{
+		{
+			name:   "test next ranged with range 5",
+			rng:    5,
+			output: 1,
+		},
+		{
+			name:   "test next ranged with range 18324",
+			rng:    18324,
+			output: 6145,
+		},
+		{
+			name:   "test next ranged with range 10000000000",
+			rng:    10000000000,
+			output: 3353954101,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			prng, err := createValidatorPRNG()
+			if err != nil {
+				t.Fatalf("cannot create validator prng: %v", err)
+			}
+			output := prng.NextRanged(test.rng)
+			if output != test.output {
+				t.Errorf("incorrect value: got %v, want %v", output, test.output)
+			}
+		})
+	}
+}
+
+func TestIncreaseSeed(t *testing.T) {
+	seed1 := [32]byte{}
+	seed1[31] = 1
+	seed40 := [32]byte{}
+	seed40[31] = 40
+	seed256 := [32]byte{}
+	seed256[30] = 1
+	tests := []struct {
+		name  string
+		times int
+		// expected seed
+		seed [32]byte
+	}{
+		{
+			name:  "test increase seed 1 time",
+			times: 1,
+			seed:  seed1,
+		},
+		{
+			name:  "test increase seed 40 time",
+			times: 40,
+			seed:  seed40,
+		},
+		{
+			name:  "test increase seed 256 time", // todo is it okay?
+			times: 256,
+			seed:  seed256,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			prng, err := createValidatorPRNG()
+			if err != nil {
+				t.Fatalf("cannot create validator prng: %v", err)
+			}
+			for i := 0; i < test.times; i++ {
+				prng.increaseSeed()
+			}
+			if !reflect.DeepEqual(prng.descr.seed, test.seed) {
+				t.Errorf("incorrect seed after increases: got %v, want %v", prng.descr.seed, test.seed)
+			}
+		})
+	}
+}
+
+func TestIncreaseHash(t *testing.T) {
+	seed32 := make([]byte, 32)
+	seed32[31] = 32
+	hash32 := []byte{
+		74, 27, 97, 202, 222, 150, 35, 10, 94, 215, 240, 213, 147, 229, 252, 235, 220, 93, 61, 153, 58, 129, 85, 207, 18, 223, 177, 238, 191, 27,
+		82, 201, 215, 138, 181, 138, 211, 64, 181, 135, 235, 229, 167, 89, 39, 106, 210, 242, 97, 239, 129, 126, 111, 113, 182, 53, 72, 200, 103,
+		177, 156, 208, 84, 20,
+	}
+	tests := []struct {
+		name string
+		seed []byte
+		// expected hash
+		hash []byte
+	}{
+		{
+			name: "test rebuild hash",
+			seed: seed32,
+			hash: hash32,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			prng, err := createValidatorPRNG()
+			if err != nil {
+				t.Fatalf("cannot create validator prng: %v", err)
+			}
+			copy(prng.descr.seed[:], test.seed)
+			prng.rebuildHash()
+			if !reflect.DeepEqual(prng.descr.hash, test.hash) {
+				t.Errorf("incorrect hash after rebuild: got %v, want %v", prng.descr.hash, test.hash)
+			}
+		})
+	}
+}

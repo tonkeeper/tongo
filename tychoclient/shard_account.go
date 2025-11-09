e2f53ce5a63b5f769290c9a7ff1f2b2a4e2c3269
callmedenchick <dvolkov@walletteam.org>
2025-11-10 00:10:17 +0100

    add ParseShardAccount

--- tychoclient/shard_account.go
+++ tychoclient/shard_account.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 
 	"github.com/tonkeeper/tongo/boc"
+	"github.com/tonkeeper/tongo/tlb"
 	"github.com/tonkeeper/tongo/tychoclient/proto"
 )
 
@@ -91,16 +92,52 @@ func (c *Client) getShardAccountInternal(ctx context.Context, workchain int32, a
 	}
 }
 
+// GetShardAccountRaw gets account state without parsing - used for debugging
+func (c *Client) GetShardAccountRaw(ctx context.Context, workchain int32, address []byte, withProof bool) (*ShardAccountInfo, error) {
+	req := &proto.GetShardAccountRequest{
+		Workchain: workchain,
+		Address:   address,
+		WithProof: withProof,
+		AtBlock: &proto.GetShardAccountRequest_Latest{
+			Latest: &proto.LatestBlock{},
+		},
+	}
+
+	resp, err := c.client.GetShardAccount(ctx, req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get shard account: %w", err)
+	}
+
+	switch result := resp.Account.(type) {
+	case *proto.GetShardAccountResponse_BlockNotFound:
+		mcInfo := result.BlockNotFound.McStateInfo
+		return nil, fmt.Errorf("block not found at MC seqno %d (LT: %d, utime: %d)",
+			mcInfo.McSeqno, mcInfo.Lt, mcInfo.Utime)
+	case *proto.GetShardAccountResponse_Accessed:
+		info := &ShardAccountInfo{
+			McStateInfo:  result.Accessed.McStateInfo,
+			AccountState: result.Accessed.AccountState,
+			Proof:        result.Accessed.Proof,
+		}
+
+		// Do not parse account state - leave ParsedAccountState as nil
+
+		return info, nil
+	default:
+		return nil, fmt.Errorf("unexpected response type: %T", result)
+	}
+}
+
 // ShardAccountInfo contains account state information
 type ShardAccountInfo struct {
 	McStateInfo        *proto.McStateInfo
-	AccountState       []byte    // BOC-encoded ShardAccount (if found)
-	Proof              []byte    // BOC-encoded collection of proofs (if withProof was true)
-	ParsedAccountState *boc.Cell // Parsed BOC cell
+	AccountState       []byte            // BOC-encoded ShardAccount (if found)
+	Proof              []byte            // BOC-encoded collection of proofs (if withProof was true)
+	ParsedAccountState *tlb.ShardAccount // Parsed TLB ShardAccount
 }
 
 // ParseShardAccount parses BOC-encoded ShardAccount data
-func ParseShardAccount(bocData []byte) (*boc.Cell, error) {
+func ParseShardAccount(bocData []byte) (*tlb.ShardAccount, error) {
 	if len(bocData) == 0 {
 		return nil, fmt.Errorf("empty BOC data")
 	}
@@ -114,8 +151,15 @@ func ParseShardAccount(bocData []byte) (*boc.Cell, error) {
 		return nil, fmt.Errorf("no cells in BOC")
 	}
 
-	// Return the first root cell which contains the ShardAccount
-	return cells[0], nil
+	var account tlb.ShardAccount
+	decoder := tlb.NewDecoder()
+	decoder.WithDebug()
+	err = decoder.Unmarshal(cells[0], &account)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal shard account: %w", err)
+	}
+
+	return &account, nil
 }
 
 // String returns a human-readable representation of ShardAccountInfo
@@ -143,7 +187,8 @@ func (s *ShardAccountInfo) String() string {
 	}
 
 	if s.ParsedAccountState != nil {
-		result += fmt.Sprintf("  Parsed Account:\n%s\n", s.ParsedAccountState.ToString())
+		result += fmt.Sprintf("  Parsed Account: available (LT: %d, Hash: %x)\n",
+			s.ParsedAccountState.LastTransLt, s.ParsedAccountState.LastTransHash)
 	}
 
 	return result
